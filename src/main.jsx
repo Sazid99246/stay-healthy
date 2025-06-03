a6ca7ac664e6e9e35fb2fca8b1ed73b206bd60a4
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-06-03 17:11:01 +0600

    Instant Consultation component added

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar.jsx'
 import Landing_Page from './Components/Landing_Page/Landing_Page.jsx'
+import InstantConsultation from './Components/Instant_Consultation/InstantConsultation.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')).render(
       <Navbar/>
       <Routes>
         <Route path="/" element={<Landing_Page />} />
+        <Route path="/instant-consultation" element={<InstantConsultation />} />
       </Routes>
     </StrictMode>
   </BrowserRouter>,
