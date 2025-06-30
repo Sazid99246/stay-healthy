7c2e4ffe1050f12723c744173fb0fe0b2e44c474
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-06-30 02:02:52 -0400

    MONGODB connection added

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:Sv2Znjr6Hw5hN9CL1eylIoJe@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
